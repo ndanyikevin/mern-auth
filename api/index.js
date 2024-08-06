4c6fe052797d833dc2b555f6ce2bc698d0521e10
ndanyikevin <ndanyikevin@gmail.com>
2024-08-06 06:34:38 +0300

    Connect to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,28 @@
 import express from "express";
+import mongoose from "mongoose";
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv'
+
+dotenv.config();
+const uri = process.env.MONGO
+
+const client =  new MongoClient(uri)
+
+async function connectToMongoDB(){
+    try{
+        await client.connect()
+        console.log('Connected to MongoDB')
+    }
+    catch(err){
+        console.error(err)
+    }
+}
+
+connectToMongoDB()
 
 const app = express()
 
 app.listen(3000, ()=> {
-    console.log('Server listening on port 3000')
+    console.log('Servey listening on port 3000')
 })
+
